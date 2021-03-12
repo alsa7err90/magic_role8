78b14e6ca888fd6a77b6c7654cbdbbf9e8426681
abdulnafea <66330574+alsa7err90@users.noreply.github.com>
2021-03-12 12:00:27 +0200

    Add files via upload

--- src/db/2021_02_19_145245_create_magroles_table.php
+++ src/db/2021_02_19_145245_create_magroles_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateMagrolesTable extends Migration
@@ -19,6 +20,24 @@ class CreateMagrolesTable extends Migration
             $table->String('slug');
             $table->timestamps();
         });
+
+        DB::table('magroles')->insert(
+            array(
+                [
+                    'name' => 'admin',
+                    'slug' => 'admin'
+                ],
+                [
+                    'name' => 'editor',
+                    'slug' => 'editor'
+                ],
+                [
+                    'name' => 'user',
+                    'slug' => 'user'
+                ]
+
+            )
+        );
     }
 
     /**
